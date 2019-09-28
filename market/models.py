10f4a60f82421cccf90d342aaa206145c30f3657
Claudio Guerra <claudevandort@gmail.com>
2019-09-27 21:47:21 -0300

    market models and migrations

--- market/models.py
+++ market/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class MarketType(models.Model):
+    name = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'market_market_type'
+
+
+class Market(models.Model):
+    name = models.CharField(max_length=50)
+    market_type = models.ForeignKey(MarketType, on_delete=models.PROTECT)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Exchange(models.Model):
+    name = models.CharField(max_length=250)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class ExchangeMarket(models.Model):
+    exchange = models.ForeignKey(Exchange, related_name='exchange_markets', on_delete=models.PROTECT)
+    market = models.ForeignKey(Market, related_name='exchange_markets', on_delete=models.PROTECT)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'market_exchange_market'
+
--- market_fetcher.py
+++ market_fetcher.py
@@ -3,15 +3,29 @@
 from keys import *
 from binance.client import Client
 from binance.websockets import BinanceSocketManager
+from datetime import datetime as dt
+
 
 client = Client(API_KEY, API_SECRET)
-market = 'BTCUSDT'
+market = 'BTCUSDT'.lower()
+intervals = ['1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M']
+stream_names = ["{}@kline_{}".format(market, i) for i in intervals]
 
 def process_message(msg):
-    k = msg['k']
-    print("[{}]: ({} - {}) {} o: {}, c: {}, h: {}, l: {}, v: {}".format(k['s'], k['t'], k['T'], msg['E'], k['o'], k['c'], k['h'], k['l'], k['v']))
+    data = msg['data']
+    k = data['k']
+    symbol, interval = k['s'], k['i']
+    time_from, time_to = dt.fromtimestamp(int(k['t'])/1000), dt.fromtimestamp(int(k['T'])/1000)
+    time_event = dt.fromtimestamp(int(data['E'])/1000)
+    open_price, close_price, high_price, low_price = k['o'], k['c'], k['h'], k['l']
+    volume, closed = k['v'], k['x']
+
+    print("[{}]({}): ({} - {}) {} o: {}, c: {}, h: {}, l: {}, v: {}, closed: {}".format(
+        symbol, interval, time_from, time_to, time_event,
+        open_price, close_price, high_price, low_price,
+        volume, closed))
 
 
 bm = BinanceSocketManager(client)
-conn_key = bm.start_kline_socket(market, process_message)
+conn_key = bm.start_multiplex_socket(stream_names, process_message)
 bm.start()
